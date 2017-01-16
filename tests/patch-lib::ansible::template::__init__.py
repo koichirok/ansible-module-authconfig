79bbc0ef33173101901c4181a68c3d3f1107678d
KIKUCHI Koichiro <koichiro@hataki.jp>
2017-01-16 17:27:03 +0900

    Enable notification to galaxy and update patch

--- tests/patch-lib::ansible::template::__init__.py
+++ tests/patch-lib::ansible::template::__init__.py
@@ -1,18 +1,31 @@
---- ansible/template/__init__.py.bak	2016-11-01 12:43:19.000000000 +0900
-+++ ansible/template/__init__.py	2016-11-29 19:52:35.239201986 +0900
-@@ -355,10 +355,12 @@
+diff --git a/lib/ansible/template/__init__.py b/lib/ansible/template/__init__.py
+index 7e0c559..f9ee02f 100644
+--- ansible/template/__init__.py
++++ ansible/template/__init__.py
+@@ -418,16 +418,24 @@ class Templar:
                  # we don't use iteritems() here to avoid problems if the underlying dict
                  # changes sizes due to the templating, which can happen with hostvars
                  for k in variable.keys():
--                    if k not in static_vars:
--                        d[k] = self.template(variable[k], preserve_trailing_newlines=preserve_trailing_newlines, fail_on_undefined=fail_on_undefined, overrides=overrides)
-+                    key = self.template(k, preserve_trailing_newlines=preserve_trailing_newlines, fail_on_undefined=fail_on_undefined, overrides=overrides)
-+
-+                    if key not in static_vars:
-+                        d[key] = self.template(variable[k], preserve_trailing_newlines=preserve_trailing_newlines, fail_on_undefined=fail_on_undefined, overrides=overrides)
++                    k_orig = k
++                    k = self.template(
++                            k,
++                            preserve_trailing_newlines=preserve_trailing_newlines,
++                            fail_on_undefined=fail_on_undefined,
++                            overrides=overrides,
++                            disable_lookups=disable_lookups,
++                        )
+                     if k not in static_vars:
+                         d[k] = self.template(
+-                                   variable[k],
++                                   variable[k_orig],
+                                    preserve_trailing_newlines=preserve_trailing_newlines,
+                                    fail_on_undefined=fail_on_undefined,
+                                    overrides=overrides,
+                                    disable_lookups=disable_lookups,
+                                )
                      else:
 -                        d[k] = variable[k]
-+                        d[key] = variable[k]
++                        d[k] = variable[k_orig]
                  return d
              else:
                  return variable
